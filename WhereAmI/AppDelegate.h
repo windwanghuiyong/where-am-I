5df0b199c1978db1a520074e404e67479cf5e9ab
王慧勇 <weifeng19911024@163.com>
2017-01-31 23:00:15 +0800

    Initial Commit

---
+++ WhereAmI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereAmI
+//
+//  Created by wanghuiyong on 31/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereAmI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereAmI
+//
+//  Created by wanghuiyong on 31/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
